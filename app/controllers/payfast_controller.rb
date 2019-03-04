68c8d18a34460289a952136ab5335f68aa8490a0
Vinchenso <chensok@gmail.com>
2019-03-04 21:59:15 +0200

    add routes and database config

---
+++ app/controllers/payfast_controller.rb
@@ -0,0 +1,11 @@
+class PayfastController < ApplicationController
+  def checkout
+  end
+  def success
+  end
+  def cancelled
+  end
+  def notify
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root 'payfast#checkout'
+
+get 'success' => 'payfast#success'
+get 'cancelled' => 'payfast#cancelled'
+post 'notify' => 'payfast#notify'
+
 end
