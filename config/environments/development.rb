6b6a4c6ae2f184a074734647f5924fdd38bb6855
Vinchenso <chensok@gmail.com>
2019-03-13 09:18:19 +0200

    change serveo to localtunnel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -5,7 +7,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.hosts << 'payfastexample.localtunnel.me'
+  config.web_console.whitelisted_ips = '41.48.37.66'
   # Do not eager load code on boot.
   config.eager_load = false
 
